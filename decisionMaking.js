c15c428bbb092f6662af320c1842d477db2b20e1
Shaya Golafshani <s.golaff@gmail.com>
2022-06-08 20:08:14 -0600

    Update answers to decision making file

--- decisionMaking.js
+++ decisionMaking.js
@@ -5,7 +5,7 @@
 
 var doorChoice = 1;
 var bearClothing = "";
-var bearChoice = 1;
+var bearChoice = 2;
 
 console.log("You enter a dark room with two doors. Do you go through #1 or #2?");
 
@@ -23,21 +23,28 @@ console.log("2. Point it out to the bear?");
 console.log("3. Make a dash for the next room?");
 
 if (bearChoice === 1) {
-  "You offer the bear your " + bearClothing + " and the bear shows you a secret passage out!";
+  console.log("You offer the bear your " + bearClothing + " and the bear shows you a secret passage out!");
 } else if (bearChoice === 2) {
-  "You tell the bear the " + bearClothing + " is too small and it starts to cry!";
+  console.log("You tell the bear the " + bearClothing + " is too small and it starts to cry!");
 } else if (bearChoice === 3) {
-  "You run as fast as you can into the next room. It's full of snakes!";
+  console.log("You run as fast as you can into the next room. It's full of snakes!");
 } else {
-  "You stay with the bear and become it's best friend!";
+  console.log("You stay with the bear and become it's best friend!");
 }
 
 
 // Questions
 // 1. In English, using technical vocabulary, describe what is happening between lines 12 and 16.
+//If the variable doorChoice is equal to 1, then the bearClothing will be a hat. Otherwise, the bearClothing will be a scarf. If you change the variable doorChoice to any other number, the bearClothing will equal scarf.
 // 2. What variable has a new value assigned to it after the first if statement executes?
+//The variable bearClothing has a new value assigned to it after the first if statement executes.
 // 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
+//The bear value would be scarf.
 // 4. In English, using technical vocabulary, describe what is happening between lines lines 25 and 33.
+// Between lines 25-33, the if statement is determining which console.log will print, depending on what the variable bearChoice is equal to.
 // 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
+// The outcome will be "You run as fast as you can into the next room. It's full of snakes!"
 // 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
+// The final outcome will be "You tell the bear the hat is too small and it starts to cry!""
 // 7. What is your favorite ending?
+//"You stay with the bear and become it's best friend!"
