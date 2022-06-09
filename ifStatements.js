2c17c4205d43c8045ef66f67c71f6be9c3d4bb82
Shaya Golafshani <s.golaff@gmail.com>
2022-06-08 21:09:53 -0600

    Update the final version of if statements

--- ifStatements.js
+++ ifStatements.js
@@ -69,8 +69,8 @@ if (numQuarters === 2) {
 var cupsOfFlour = 1;
 var hasSauce = true;
 
-if (cupsOfFlour >= 2 && hasSauce = true) {
+if (cupsOfFlour >= 2 && hasSauce === true) {
   console.log("I can make pizza.");
-} else if (cupsOfFlour < 2 || hasSauce = false) {
+} else {
   console.log("I cannot make pizza.");
 }
